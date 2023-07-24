c69b81e02d33e2ed60ad6930811df859316b9a62
mohammedaneesuddin <139983071+mohammedaneesuddin@users.noreply.github.com>
2023-07-24 10:55:14 +0530

    2 files modified, 4 added

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+@NgModule({
+  imports:      [ 
+  ],
+  declarations: [ AppComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
